624454df422ba403013c58fadd1c32db861aae09
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2020-03-27 15:58:22 -0400

    Resolving schema construction

--- lib/lost_ship/schema.rb
+++ lib/lost_ship/schema.rb
@@ -1,27 +1,28 @@
 require 'dry/schema'
 module LostShip
+  begin
   Schema = Dry::Schema.JSON do
     optional(:threat).hash do
       required(:round).filled(:integer, included_in?: (0..5).to_a)
-      required(:ships).array.schema do
+      required(:ships).array(:hash) do
         required(:type).filled(:integer, included_in?: (1..3).to_a)
         required(:damage).filled(:integer, included_in?: (0..8).to_a)
       end
     end
     required(:fleet).hash do
-      optional(:formation).array.schema do
-        required(:pilot_index).filled(:integer, gte?: 0)
-        required(:scout_index).filled(:integer, gte?: 0)
+      optional(:formation).array(:hash) do
+        required(:pilot_index).filled(:integer, gteq?: 0)
+        required(:scout_index).filled(:integer, gteq?: 0)
       end
       required(:ship).hash do
         required(:name).filled(:string)
-        required(:leaps_since_incident).filled(:integer, gte?: 0)
-        required(:fuel).filled(:integer, gte?: 0)
-        required(:parts).filled(:integer, gte?: 0)
-        required(:bscore).filled(:integer, gte?: 0)
+        required(:leaps_since_incident).filled(:integer, gteq?: 0)
+        required(:fuel).filled(:integer, gteq?: 0)
+        required(:parts).filled(:integer, gteq?: 0)
+        required(:bscore).filled(:integer, gteq?: 0)
         required(:hull).hash do
           required(:damage).value(:integer, included_in?: (0..7).to_a)
-          required(:upgraded).filled(:boolean)
+          required(:upgraded).filled(:bool?)
         end
         required(:new_recruits).filled(:integer, included_in?: (0..6).to_a)
         required(:halfway_trained).filled(:integer, included_in?: (0..6).to_a)
@@ -29,23 +30,23 @@ module LostShip
         SHIP_COMPONENT_STATUS_RANGE = [0,33,66,100]
         required(:engines).hash do
           required(:status).filled(:integer, included_in?: SHIP_COMPONENT_STATUS_RANGE)
-          required(:upgraded).filled(:boolean)
+          required(:upgraded).filled(:bool?)
         end
         required(:mining_laser).hash do
           required(:status).filled(:integer, included_in?: SHIP_COMPONENT_STATUS_RANGE)
-          required(:upgraded).filled(:boolean)
+          required(:upgraded).filled(:bool?)
         end
         required(:scout_bay).hash do
           required(:status).filled(:integer, included_in?: SHIP_COMPONENT_STATUS_RANGE)
-          required(:upgraded).filled(:boolean)
+          required(:upgraded).filled(:bool?)
         end
         required(:sick_back).hash do
           required(:status).filled(:integer, included_in?: SHIP_COMPONENT_STATUS_RANGE)
-          required(:upgraded).filled(:boolean)
+          required(:upgraded).filled(:bool?)
         end
         required(:sensors).hash do
           required(:status).filled(:integer, included_in?: SHIP_COMPONENT_STATUS_RANGE)
-          required(:upgraded).filled(:boolean)
+          required(:upgraded).filled(:bool?)
         end
       end
       SCOUT_STATUS_DESTROYED = 0
@@ -58,7 +59,7 @@ module LostShip
         SCOUT_STATUS_FIFTY_PERCENT,
         SCOUT_STATUS_OPERATIONAL
       ]
-      required(:scouts).array.schema do
+      required(:scouts).array(:hash) do
         required(:name).filled(:string, max_size?: 10)
         required(:damage).filled(:integer, included_in?: SCOUT_STATUS_RANGE)
       end
@@ -70,12 +71,16 @@ module LostShip
         PILOT_STATUS_INJURED,
         PILOT_STATUS_KILL_IN_ACTION
       ]
-      required(:pilots).array.schema do
+      required(:pilots).array(:hash) do
         required(:name).filled(:string, max_size?: 10)
-        required(:kills).filled(:integer, gte?: 0)
+        required(:kills).filled(:integer, gteq?: 0)
         required(:status).filled(:integer, included_in?: PILOT_STATUS_RANGE)
-        required(:leaps_injured).optional(:integer, gte?: 0)
+        optional(:leaps_injured).filled(:integer, gteq?: 0)
       end
     end
   end
+rescue => e
+  puts e.backtrace
+  raise e
+end
 end
