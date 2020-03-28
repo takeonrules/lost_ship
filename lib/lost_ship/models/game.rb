c2cfc740b53e892077532e46f74b93c2ab354738
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2020-03-28 17:06:39 -0400

    Adding a Game.start method

--- lib/lost_ship/models/game.rb
+++ lib/lost_ship/models/game.rb
@@ -5,6 +5,15 @@ require 'lost_ship/models/fleet'
 module LostShip
   module Models
     class Game < Dry::Struct
+      def self.start(ship_name:, scout_names:, pilot_names:)
+        new(
+          fleet: {
+            ship: { name: ship_name },
+            scouts: Array(scout_names).map { |name| { name: name } },
+            pilots: Array(pilot_names).map { |name| { name: name } }
+          }
+        )
+      end
       transform_keys(&:to_sym)
       attribute? :threat, Types.Constructor(Threat)
       attribute :fleet, Types.Constructor(Fleet)
