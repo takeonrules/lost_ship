e22c60681fe10a62cfd1ac4085d9ce12e7ef045e
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2020-03-28 17:15:34 -0400

    Adding Game.write!

--- lib/lost_ship/models/game.rb
+++ lib/lost_ship/models/game.rb
@@ -2,6 +2,8 @@ require 'dry/struct'
 require 'lost_ship/models/types'
 require 'lost_ship/models/threat'
 require 'lost_ship/models/fleet'
+require 'dry/cli/utils/files'
+require 'json'
 module LostShip
   module Models
     class Game < Dry::Struct
@@ -14,6 +16,7 @@ module LostShip
           }
         )
       end
+
       transform_keys(&:to_sym)
       attribute? :threat, Types.Constructor(Threat)
       attribute :fleet, Types.Constructor(Fleet)
@@ -21,6 +24,11 @@ module LostShip
       Schema = Dry::Schema.JSON do
         optional(:threat).hash(Threat::Schema)
         required(:fleet).hash(Fleet::Schema)
+      end
+
+      def write!(filename:)
+        content = JSON.dump(self.to_h)
+        Dry::CLI::Utils::Files.write(filename, content)
       end
     end
   end
